be094fd831ec77a38b3a5048c12a749b49178f48
ansar <ansarsaudagar40@gmail.com>
2023-11-25 13:57:23 +0530

    top nav ui completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatIconModule],
+  imports: [CommonModule, RouterOutlet, MatIconModule, TopNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
