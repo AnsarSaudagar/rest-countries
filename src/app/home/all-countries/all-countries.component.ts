ca9325cc03432cbe7d4ab9f9307e9c20173c5833
ansar <ansarsaudagar40@gmail.com>
2023-11-26 00:51:17 +0530

    home ui ready

---
+++ src/app/home/all-countries/all-countries.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-all-countries',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './all-countries.component.html',
+  styleUrl: './all-countries.component.css'
+})
+export class AllCountriesComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchNavComponent } from './search-nav/search-nav.component';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SearchNavComponent],
+  imports: [CommonModule, SearchNavComponent, AllCountriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
