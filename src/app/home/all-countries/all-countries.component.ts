3728beca526f3f1646e58a75200ff21858f38702
ansar <ansarsaudagar40@gmail.com>
2023-11-26 16:43:24 +0530

    added

--- src/app/home/all-countries/all-countries.component.ts
+++ src/app/home/all-countries/all-countries.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-all-countries',
   standalone: true,
   imports: [CommonModule],
+  providers: [DataService],
   templateUrl: './all-countries.component.html',
   styleUrl: './all-countries.component.css'
 })
-export class AllCountriesComponent {
+export class AllCountriesComponent implements OnInit {
+
+  countries: any[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  countryObserver: any = {
+    next: (countries: any) => this.countries = countries,
+    error: (err: any) => console.log(err)
+  }
+
+  ngOnInit() {
+    this.dataService.getAllCountries().subscribe(this.countryObserver);
+  }
+
 
 }
